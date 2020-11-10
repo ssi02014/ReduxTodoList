4df83543d4cac94066eff21090cead2efae67fd1
JeonMinJae <ssi02014@naver.com>
2020-11-10 17:12:58 +0900

    Redux TodoList

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <App />
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/modules/todos.js
@@ -0,0 +1,58 @@
+import { createAction, handleActions } from "redux-actions";
+
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; //인풋 값을 변경
+const INSERT = 'todos/INSERT'; //새로운 todo를 등록
+const TOGGLE = 'todos/TOGGLE'; //todo를 체크/ 체크 해제
+const REMOVE = 'todos/REMOVE'; //todo를 제거
+
+export const changeInput = createAction(CHANGE_INPUT, input => input);
+
+let id = 3; //insert가 호출될 때마다 1씩 더해짐
+
+export const insert = createAction(INSERT, text => ( {
+    id : id++,
+    text,
+    done: false
+}));
+
+export const toggle = createAction(TOGGLE, id => id);
+
+export const remove = createAction(REMOVE, id => id);
+
+const initialState = {
+    input : '',
+    todos : [
+        {
+            id: 1,
+            text: '리덕스 기초 배우기',
+            done : true
+        },
+        {
+            id:2,
+            text: '리액트와 리덕스 사용하기',
+            done: false
+        }
+    ]
+};
+
+const todos = handleActions(
+    {
+        [CHANGE_INPUT]: (state,{payload : input}) => ({ ...state, input}),
+        [INSERT]: (state, {payload : todo}) => ({
+            ...state,
+            todos: state.todos.concat(todo),
+        }),
+        [TOGGLE]: (state, {payload : id}) => ({
+            ...state,
+            todos: state.todos.map(todo =>
+                todo.id === id ? { ...todo, done: !todo.done}: todo),
+        }),
+        [REMOVE]: (state, {payload : id}) => ({
+            ...state,
+            todos: state.todos.filter(todo => todo.id !== id)
+        }),
+    },
+    initialState,
+);
+
+export default todos;
